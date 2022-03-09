811fc5f2e59c9ab6f412674b872cd3aa747a953d
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-03-09 13:20:46 +0100

    Add RandomWalkIdentities class #1
    Add random walk identities class which takes all possible combinations of the fixed factors (combinations of fixed factors form an identity) and creates a random walk for each. Still need to implement multiple random walks per identity.

--- lsbd_vae/data/random_walk_factor.py
+++ lsbd_vae/data/random_walk_factor.py
@@ -4,21 +4,21 @@ import numpy as np
 import os
 
 
-def get_random_initial(num_paths: int, max_values) -> tf.Tensor:
-    return tf.stack([tf.random.uniform((num_paths, 1), maxval=maxval, dtype=tf.int32) for maxval in max_values],
-                    axis=-1)
-
-
 class RandomWalkFactor:
-    def __init__(self, source_path: str, factor_values: List, num_random_walks: int,
+    """
+    Class that can be used to create tf.data.Dataset that loads images within a source_path with naming structure
+    factor1_factor2..._factornumfactor.extension by doing num_random_walks of length random_walk_length
+    across the bool_change_factors. Class stores the paths corresponding to those images
+    """
+
+    def __init__(self, source_path: str, factor_values_list: List, num_random_walks: int,
                  step_sizes: np.array,
                  random_walk_length: int, bool_change_factors: List[bool], image_shape: Tuple[int, int, int],
                  extension: str = ".png", labeling_function: Optional = None, seed: Optional = None):
         """
-        Abstract class that can be used to create tf.data.Dataset that loads images with naming structure
-        factor1_factor2..._factornumfactor.png
+        Initialize class
         :param source_path: path were images are stored
-        :param factor_values: Possible factor values used in the names of the saved images
+        :param factor_values_list: Possible factor values used in the names of the saved images
         :param num_random_walks: Number of random walks created
         :param step_sizes: Size of steps for each factor
         :param random_walk_length: Number of steps taken in each random walk
@@ -29,12 +29,12 @@ class RandomWalkFactor:
         """
         tf.random.set_seed(seed)
         # Factor variables
-        self.factor_values = factor_values
-        self.num_factors = len(factor_values)
+        self.factor_values_list = factor_values_list
+        self.num_factors = len(factor_values_list)
 
         # Random walk variables
         self.num_random_walks = num_random_walks
-        self.max_factor_indexes = np.array([len(factor) for factor in factor_values]).astype(np.int32)
+        self.max_factor_indexes = np.array([len(factor) for factor in factor_values_list]).astype(np.int32)
         self.random_walk_length = random_walk_length
         self.bool_change_factors = bool_change_factors
         self.num_changing_factors = sum(bool_change_factors)
@@ -54,14 +54,31 @@ class RandomWalkFactor:
         # Assertions
         assert np.ndim(bool_change_factors) == 1, "Dimension of mask should be 1"
         assert len(bool_change_factors) == len(
-            factor_values), "Length of mask should be the same as the length of factor values"
+            factor_values_list), "Length of mask should be the same as the length of factor values"
 
         assert len(self.step_sizes) == self.num_factors
 
-        self.paths, self.transformations, self.labels = self._generate_image_paths_transformations()
+        self.paths, self.transformations, self.labels = self.__generate_image_paths_transformations()
         tf.random.set_seed(None)
 
-    def _get_random_step_sign(self):
+
+
+    @property
+    def num_random_walks(self):
+        return self.__num_random_walks
+
+    @num_random_walks.setter
+    def num_random_walks(self, value):
+        self.__num_random_walks = value
+
+    def get_random_initial(self) -> tf.Tensor:
+        initial_factors = []
+        for max_val in self.max_factor_indexes:
+            random_initial = tf.random.uniform((self.num_random_walks, 1), maxval=max_val, dtype=tf.int32)
+            initial_factors.append(random_initial)
+        return tf.stack(initial_factors, axis=-1)
+
+    def __get_random_step_sign(self):
         """
         Returns random +/- sign for factors that change
         :return:
@@ -70,7 +87,7 @@ class RandomWalkFactor:
         sign = tf.expand_dims(tf.where(sign == 0, x=tf.ones_like(sign), y=-tf.ones_like(sign)), axis=-1)
         return sign
 
-    def _get_random_factors(self):
+    def __get_random_factors(self):
         """
         Samples uniform random values from factors that change
         :return:
@@ -78,7 +95,7 @@ class RandomWalkFactor:
         return tf.random.uniform((self.num_random_walks, self.random_walk_length - 1), maxval=self.num_changing_factors,
                                  dtype=tf.int32)
 
-    def _get_paths(self, factor_states):
+    def __get_paths(self, factor_states):
         """
         Takes factor_states and produces paths to the corresponding images
         :param factor_states: shape (batch_size, n_steps, n_factors)
@@ -88,7 +105,7 @@ class RandomWalkFactor:
         gathered_factors = []
         for num_factor in range(self.num_factors):
             # Gather factor values according to the corresponding factor states
-            factor_values = tf.gather(self.factor_values[num_factor], factor_states[..., num_factor])
+            factor_values = tf.gather(self.factor_values_list[num_factor], factor_states[..., num_factor])
             gathered_factors.append(factor_values)
         # Join states in string
         filenames = tf.strings.join(gathered_factors, separator="_")
@@ -98,7 +115,7 @@ class RandomWalkFactor:
         paths = tf.strings.join([self.source_path, filenames], separator=os.sep)
         return paths
 
-    def _get_steps(self, random_factors, sign):
+    def __get_steps(self, random_factors, sign):
         """
         Creates the steps in the factor space by taking the sampled random factors and the random sign values
         :param random_factors: random factor identifiers representing the factor that changes in a given step
@@ -116,26 +133,25 @@ class RandomWalkFactor:
                 steps_list.append(steps[..., changing_factor_id])
                 changing_factor_id += 1
             else:
-                steps_list.append(tf.zeros((self.num_random_walks, self.random_walk_length - 1), dtype=tf.int32))
+                steps_list.append(tf.zeros((len(sign), self.random_walk_length - 1), dtype=tf.int32))
         steps = tf.stack(steps_list, axis=-1)
         steps = tf.expand_dims(tf.expand_dims(self.step_sizes, axis=0), axis=0) * sign * steps
         steps = tf.concat([tf.zeros((self.num_random_walks, 1, self.num_factors), dtype=tf.int32), steps], axis=1)
         return steps
 
-    def _generate_image_paths_transformations(self):
+    def __generate_image_paths_transformations(self):
         """
         Creates the paths to the images, the transformations and provides the labels for the random walks
         :return:
         """
         # Starting point for each of the random walks
-        starting_point = get_random_initial(self.num_random_walks,
-                                            self.max_factor_indexes)  # (batch_size, num_steps, num_factors)
+        starting_point = self.get_random_initial()  # (num_random_walks, num_steps, num_factors)
         # Get random factors modified in each step of the random walk
-        random_factors = self._get_random_factors()  # (batch_size, num_steps, 1)
+        random_factors = self.__get_random_factors()  # (batch_size, num_steps, 1)
         # Get random +/- sign for each step of the random walk
-        sign = self._get_random_step_sign()  # (batch_size, num_steps, 1)
+        sign = self.__get_random_step_sign()  # (batch_size, num_steps, 1)
         # Get the step values for each factor
-        steps = self._get_steps(random_factors, sign)  # (bath_size, num_steps, num_factors)
+        steps = self.__get_steps(random_factors, sign)  # (bath_size, num_steps, num_factors)
         # Accumulate the steps
         random_relative_steps = tf.cumsum(steps, axis=1)  # (batch_size, num_steps, num_factors)
         # Transform initial state
@@ -150,14 +166,14 @@ class RandomWalkFactor:
         transformations = tuple([angles_relative[:, :, i:i + 1] for i in range(self.num_factors)])
 
         # Get image paths
-        paths = self._get_paths(final_states)
+        paths = self.__get_paths(final_states)
         if self.labeling_function is None:
             labels = None
         else:
             labels = self.labeling_function(paths)
         return paths, transformations, labels
 
-    def _get_image_load_function(self, batch_size: int, get_labels: bool = False, flatten: bool = False):
+    def __get_image_load_function(self, batch_size: int, get_labels: bool = False, flatten: bool = False):
         """
         Create function to apply to tf.data.Dataset data
         :param batch_size: batch size output of the tf.data.Dataset
@@ -217,9 +233,65 @@ class RandomWalkFactor:
                 {"paths": self.paths, "transformations": self.transformations, "labels": self.labels})
         ds = ds.batch(batch_size, drop_remainder=True)
         ds = ds.map(
-            self._get_image_load_function(batch_size, get_labels=self.labeling_function is not None, flatten=flatten))
+            self.__get_image_load_function(batch_size, get_labels=self.labeling_function is not None, flatten=flatten))
         # ds = ds.cache()
         ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
         if shuffle != 0:
             ds = ds.shuffle(shuffle)
         return ds
+
+
+class RandomWalkIdentities(RandomWalkFactor):
+    """
+    Class that can be used to create tf.data.Dataset that loads images within a source_path with naming structure
+    factor1_factor2..._factornumfactor.extension by doing random walks of length random_walk_lengthfor several
+    fixed identities corresponding to all combinations of the bool_fixed_factors.
+    Class stores the paths corresponding to those images.
+    """
+
+    def __init__(self, source_path: str, factor_values_list: List, num_random_walks: int,
+                 step_sizes: np.array,
+                 random_walk_length: int, bool_change_factors: List[bool], image_shape: Tuple[int, int, int],
+                 extension: str = ".png", labeling_function: Optional = None, seed: Optional = None):
+        self.bool_fixed_factors = [not change_factor for change_factor in bool_change_factors]
+        self.fixed_indexes_meshgrid = self.__get_fixed_indexes_meshgrid(factor_values_list)
+
+        super(RandomWalkIdentities, self).__init__(source_path, factor_values_list, num_random_walks, step_sizes,
+                                                   random_walk_length, bool_change_factors, image_shape,
+                                                   extension, labeling_function, seed)
+
+    @property
+    def num_random_walks(self):
+        return self.__num_random_walks
+
+    @num_random_walks.setter
+    def num_random_walks(self, value):
+        value = 1 #TODO: Fix to add functionality for multiple random walks per identity
+        self.__num_random_walks = len(self.fixed_indexes_meshgrid[-1]) * value
+
+    def __get_fixed_indexes_meshgrid(self, factor_values_list):
+        fixed_factors = []
+        for num_factor, factor_values in enumerate(factor_values_list):
+            if self.bool_fixed_factors[num_factor]:
+                fixed_factors.append(np.arange(len(factor_values)))
+        print(len(fixed_factors))
+        meshgrids = np.meshgrid(*fixed_factors)
+        fixed_factors_meshgrid = []
+        for grid in meshgrids:
+            fixed_factors_meshgrid.append(grid.reshape(np.product(grid.shape)))
+        return fixed_factors_meshgrid
+
+    def get_random_initial(self) -> tf.Tensor:
+        initial_factors = []
+        num_fixed = 0
+        for num_factor, bool_change in enumerate(self.bool_change_factors):
+            if bool_change:
+                random_initial = tf.random.uniform((self.num_random_walks, 1),
+                                                   maxval=self.max_factor_indexes[num_factor], dtype=tf.int32)
+                initial_factors.append(random_initial)
+                print("Random", random_initial.shape)
+            else:
+                initial_factors.append(np.expand_dims(self.fixed_indexes_meshgrid[num_fixed], axis=-1))
+                print("Fixed", np.expand_dims(self.fixed_indexes_meshgrid[num_fixed], axis=-1).shape)
+                num_fixed += 1
+        return tf.stack(initial_factors, axis=-1)
