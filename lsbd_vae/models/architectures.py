fd9b9d85aa70cba7046a597a329563a83b3938bd
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-06-14 12:52:57 +0200

    Remove unused architecture from architectures file #12

--- lsbd_vae/models/architectures.py
+++ lsbd_vae/models/architectures.py
@@ -15,9 +15,7 @@ def get_encoder_decoder(architecture: str, image_shape: Tuple[int, int, int], la
         architecture_function = encoder_decoder_dense
 
     elif architecture == "dislib":
-        architecture_parameters = {"height": image_shape[0],
-                                   "width": image_shape[1],
-                                   "depth": image_shape[2],
+        architecture_parameters = {"input_shape": image_shape,
                                    "latent_dim": latent_dim}
         architecture_function = encoder_decoder_dislib_2d
     elif architecture == "resnet50v2_dense":
@@ -60,13 +58,6 @@ def get_encoder_decoder(architecture: str, image_shape: Tuple[int, int, int], la
             encoder.trainable = True
             [_, decoder] = encoder_decoder_dense(**decoder_params)
             return encoder, decoder
-
-    elif architecture == "simple_cnn":
-        architecture_parameters = {"input_shape": image_shape,
-                                   "features": 96,
-                                   "latent_dim": latent_dim}
-        architecture_function = get_encoder_decoder_simple_cnn
-
     else:
         raise ValueError(f"{architecture} not defined")
 
@@ -91,8 +82,8 @@ def encoder_decoder_dense(latent_dim: int, input_shape: Tuple = (28, 28, 1), act
         h = Dense(units, activation=activation)(h)
         h = BatchNormalization()(h)
     h = Dense(np.prod(input_shape), activation=None)(h)
-    x_reconstr = Reshape(input_shape)(h)
-    decoder = Model(dec_in, x_reconstr)
+    x_reconstruction = Reshape(input_shape)(h)
+    decoder = Model(dec_in, x_reconstruction)
 
     return encoder, decoder
 
@@ -104,24 +95,27 @@ def encoder_decoder_vgglike_2d(latent_dim: int, input_shape: Tuple = (64, 64, 1)
     Simple but general convolutional architecture. Uses fixed kernel_size & pool_size. Customisable nr of conv2d layers,
     optional dense layers. Uses MaxPooling and UpSampling.
     """
-    n_conv_layers = len(filters_lst)
+    # Initialize the relevant variables
     height, width, depth = input_shape
-    # calculate sizes after convolutions (before dense layers)
-    conv_height = height // (pool_size ** n_conv_layers)
-    conv_width = width // (pool_size ** n_conv_layers)
-    conv_depth = filters_lst[-1]
-    assert height == conv_height * (pool_size ** n_conv_layers), "height must be a multiple of pool_size^n_conv_layers"
+    n_conv_layers = len(filters_lst)  # number of convolutional layers
+    # calculate sizes after convolutions (before the dense layers)
+    conv_depth = filters_lst[-1]  # depth after convolutions
+    conv_width = width // (pool_size ** n_conv_layers)  # width after convolutions
+    conv_height = height // (pool_size ** n_conv_layers)  # height after convolutions
+
     assert width == conv_width * (pool_size ** n_conv_layers), "width must be a multiple of pool_size^n_conv_layers"
+    assert height == conv_height * (pool_size ** n_conv_layers), "height must be a multiple of pool_size^n_conv_layers"
 
     # ENCODER
-    x = Input(shape=input_shape)
+    x = Input(shape=input_shape)  # input layer
     # convolutional layers
-    h = x
+    h = x  # input to first convolutional layer
     for i in range(n_conv_layers):
         h = Conv2D(filters=filters_lst[i], kernel_size=kernel_size, strides=(1, 1), padding="same",
                    activation=activation)(h)
         h = MaxPooling2D(pool_size=pool_size, padding="same")(h)
     # dense layers
+    # noinspection RepeatElement
     h = Flatten()(h)
     for units in dense_units_lst:
         h = Dense(units, activation=activation)(h)
@@ -190,47 +184,8 @@ def encoder_decoder_dislib_2d(latent_dim: int, input_shape: Tuple = (64, 64, 1),
     for i in reversed(range(1, n_conv_layers)):
         h = Conv2DTranspose(filters=filters_lst[i - 1], kernel_size=kernel_size, strides=strides, padding="same",
                             activation=activation)(h)
-    x_reconstr = Conv2DTranspose(filters=depth, kernel_size=kernel_size, strides=strides, padding="same",
-                                 activation="sigmoid")(h)
-    decoder = Model(dec_in, x_reconstr)
-
-    return encoder, decoder
-
-
-def get_encoder_decoder_simple_cnn(input_shape, latent_dim, features=96):
-    im_input = Input(shape=input_shape)
-    C1 = Conv2D(features, 5, padding='valid', activation='relu')(im_input)
-    P1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="valid")(C1)
-
-    C2 = Conv2D(int(features / 2), 5, padding='valid', activation='relu')(P1)
-    P2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="valid")(C2)
-
-    C3 = Conv2D(int(features / 4), 5, padding='valid', activation='relu')(P2)
-    P3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="valid")(C3)
-
-    flat = Flatten()(P3)
-    encoder = Model(im_input, flat)
-
-    # Decoder
-
-    spat_dims = P3.shape.as_list()[1:]
-
-    latent_input = Input(shape=(latent_dim,))
-    H1 = Dense(np.prod(spat_dims), activation='relu')(latent_input)
-    square = Reshape(spat_dims)(H1)
-
-    TP1 = UpSampling2D(size=(2, 2))(square)
-    CT1 = Conv2D(int(features / 2), 7, padding='valid', activation='relu')(TP1)
-
-    TP2 = UpSampling2D(size=(2, 2))(CT1)
-    CT2 = Conv2D(features, 7, padding='valid', activation='relu')(TP2)
-
-    TP3 = UpSampling2D(size=(2, 2))(CT2)
-    CT3 = Conv2D(features, 7, padding='valid', activation='relu')(TP3)
-
-    TP4 = UpSampling2D(size=(2, 2))(CT3)
-    CT4 = Conv2D(input_shape[-1], 7, padding='valid', activation='tanh', name='recons')(TP4)
-
-    decoder = Model(latent_input, CT4)
+    x_reconstruction = Conv2DTranspose(filters=depth, kernel_size=kernel_size, strides=strides, padding="same",
+                                       activation="sigmoid")(h)
+    decoder = Model(dec_in, x_reconstruction)
 
     return encoder, decoder
