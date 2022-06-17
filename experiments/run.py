f711e0800f4a911292b92ddf816388eee9235140
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-06-17 14:09:18 +0200

    Update run code #12
    Update the run code which can train with both semi-supervised and with data generated from random walk paths over the factor space.

--- experiments/run.py
+++ experiments/run.py
@@ -6,36 +6,34 @@ import matplotlib.pyplot as plt
 import argparse
 import pickle
 
-print(os.getcwd())
 sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), "lsbd_vae"))
 from lsbd_vae.utils.model_utils import get_autoencoder_model, get_ls_list
 from lsbd_vae.models.architectures import get_encoder_decoder
-from lsbd_vae.metrics.dlsbd_metric import dlsbd, create_combinations_k_values_range
+from lsbd_vae.metrics.dlsbd_metric import dlsbd, create_combinations_omega_values_range
 from lsbd_vae.utils import plotting
 from lsbd_vae.data_utils.data_loader import load_factor_data
 from experiments.configs.paper_dataset_parameters import return_data_parameters
 from lsbd_vae.data_utils.factor_dataset import FactorImageDataset
 
-TORUS_DATASETS = {"arrow", "pixel4", "modelnet", "pixel8", "pixel16"}
+TORUS_DATASETS = {"arrow", "pixel4", "modelnet_colors", "pixel8", "pixel16"}
 
 tf.config.run_functions_eagerly(True)
 
 # region ARGUMENTS SCRIPT
-parser = argparse.ArgumentParser(description="Training of LSBDVAE model either using paths or with semi-supervised pairs")
+parser = argparse.ArgumentParser(
+    description="Training of LSBDVAE model either using paths or with semi-supervised pairs")
 parser.add_argument("--gpu", nargs="?", dest="gpu_num", type=str, default="0", help="gpu to be used")
-parser.add_argument("--n_labels", nargs="+", dest='n_labels', type=int, default=[0],
-                    help="List of number of labels used for training the model")
 parser.add_argument("--dataset", nargs="?", dest="dataset", type=str, default="", help="Dataset to be used")
 parser.add_argument("--architecture", nargs="?", dest="architecture", type=str, default="dis_lib",
                     help="Architecture of model")
 parser.add_argument("--tag", nargs="?", dest="tag", type=str, default="test",
                     help="Experiment tag used to save")
-parser.add_argument("--npaths", nargs="?", dest="npaths", type=int, default=1000,
+parser.add_argument("--npaths", nargs="?", dest="npaths", type=int, default=50,
                     help="Number of paths")
-parser.add_argument("--lpaths", nargs="?", dest="lpaths", type=int, default=2,
+parser.add_argument("--lpaths", nargs="?", dest="lpaths", type=int, default=100,
                     help="Length of paths")
-parser.add_argument("--spaths", nargs="?", dest="spaths", type=int, default=1,
+parser.add_argument("--spaths", nargs="?", dest="spaths", type=int, default=3,
                     help="Step size of paths")
 parser.add_argument("--epochs", nargs="?", dest="epochs", type=int, default=100,
                     help="Training epochs")
@@ -43,14 +41,19 @@ parser.add_argument("--batch_size", nargs="?", dest="batch_size", type=int, defa
                     help="Batch size")
 parser.add_argument("--model_type", nargs="?", dest="model_type", type=str, default="LSBDVAE",
                     help="Autoencoder model type")
+parser.add_argument("--logt", nargs="?", dest="logt", type=int, default=-5,
+                    help="Clipping value for the upper bound of the log-scale")
+parser.add_argument("--distweight", nargs="?", dest="distweight", type=float, default=1,
+                    help="Value for the weighting factor of the equivariance loss in the LSBDVAE")
 parser.add_argument('--trainpath', dest="trainpath", action='store_true')
+
 parser.add_argument('--no-trainpath', dest="trainpath", action='store_false')
 parser.set_defaults(trainpath=True)
 parser.add_argument("--npairs", dest="npairs", type=int, default=0,
                     help="When trainpath is False, then train semi-supervised using npairs of transform-labelled data")
 
 args = parser.parse_args()
-# print(os.environ["CUDA_VISIBLE_DEVICES"])
+print(args)
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_num
 # endregion
 base_result_path = "./experiments/results"
@@ -67,7 +70,8 @@ if args.dataset in TORUS_DATASETS:
         "latent_types": ["s", "s"],
         "latent_dims": [1, 1],
         "kl_weights": None,
-        "kwargs_list": [{"log_t_limit": (-10, -5)}, {"log_t_limit": (-10, -5)}]
+        "kwargs_list": [{"log_t_limit": (-10, args.logt), "dist_weight": args.distweight},
+                        {"log_t_limit": (-10, args.logt), "dist_weight": args.distweight}]
     }
 else:
     latent_dim = 7
@@ -75,7 +79,8 @@ else:
         "latent_types": ["s", "e"],
         "latent_dims": [1, 5],
         "kl_weights": None,
-        "kwargs_list": [{"log_t_limit": (-10, -5)}, {}]
+        "kwargs_list": [{"log_t_limit": (-10, args.logt), "dist_weight": args.distweight},
+                        {"dist_weight": args.distweight}]
     }
 # endregion
 
@@ -86,14 +91,18 @@ if args.dataset in TORUS_DATASETS:
     if args.trainpath:
         x_l, x_l_transformations = data_class.random_paths_torus(args.npaths, args.lpaths, args.spaths)
         x_u = np.array([])
+        n_transforms = args.lpaths
     else:
+        n_transforms = args.npairs
         x_l, x_l_transformations, x_u = data_class.setup_circles_dataset_labelled_pairs(args.npairs)
 else:
     if args.trainpath:
         x_l, x_l_transformations = data_class.random_paths_cylinder(args.npaths, args.lpaths, args.spaths)
         x_u = np.array([])
+        n_transforms = args.lpaths
     else:
         x_l, x_l_transformations, x_u = data_class.setup_cylinder_dataset_labelled_pairs(args.npairs)
+        n_transforms = args.npairs
 
 print("Unlabeled shape", x_u.shape)
 print("Labeled shape", x_l.shape, "Transformation shape 1", x_l_transformations[0].shape)
@@ -102,7 +111,7 @@ print("Labeled shape", x_l.shape, "Transformation shape 1", x_l_transformations[
 # region MODEL INITIALIZATION
 model_parameters = {
     "model_type": args.model_type,
-    "n_transforms": args.lpaths,
+    "n_transforms": n_transforms,
     "input_shape": data_class.image_shape
 }
 architecture_params = {
@@ -194,11 +203,11 @@ if args.dataset in TORUS_DATASETS:
 # region LSBD METRIC
 print("Start calculation of LSBD metric")
 if args.dataset in TORUS_DATASETS:
-    k_values = create_combinations_k_values_range(start_value=-10, end_value=10)
-    lsbd_score, _ = dlsbd(latent_reps, k_values, be_verbose=True, factor_manifold="torus")
+    omega_values = create_combinations_omega_values_range(start_value=-10, end_value=10)
+    lsbd_score, _ = dlsbd(latent_reps, omega_values, be_verbose=True, factor_manifold="torus")
 else:
-    k_values = np.arange(-10, 11)
-    lsbd_score, _ = dlsbd(latent_reps, k_values, be_verbose=True, factor_manifold="cylinder")
+    omega_values = np.arange(-10, 11)
+    lsbd_score, _ = dlsbd(latent_reps, omega_values, be_verbose=True, factor_manifold="cylinder")
 
 # Saving folder
 np.save(os.path.join(experiment_metrics_save_path, "lsbd.npy"), lsbd_score)
